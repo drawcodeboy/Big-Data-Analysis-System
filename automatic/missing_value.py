a3852b09ad5dfb3dfca72555843e558eb1102c3d
drawcodeboy <20rnjsekdns@naver.com>
2024-05-07 01:10:55 +0900

    add: Work on Module

---
+++ automatic/missing_value.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+
+class AutomaticProcess():
+    def __init__(self):
+        r"""
+        drop_features: 어떠한 Case로 드랍되었는지를 딕셔너리로 나타내고,
+                        경우에 해당하는 Key에 대해서 리스트에 append한다.
+
+        """
+        self.drop_features = {
+            'drop_column': [],
+            'drop_row': [],
+        }
+
+    def missing_value_process(self, df: pd.DataFrame,
+                              upper_threshold=0.5: float,
+                              lower_threshold=0.1: float):
+        r"""
+        Args
+            df: Missing Value를 처리할 DataFrame,
+            lower_threshold: 해당 기준 값 이하는 
+        """
+        for col in df.columns:
+            missing_value_ratio = df[col].isnull().sum()/len(df[col])
+            if missing_value_ratio >= upper_threshold:
+                df.drop(col, axis=1, inplace=True)
+                self.drop_features['drop_column'].append(col)
+            elif missing_value_ratio <= lower_threshold:
+                df.drop(axis=0, inplace=True)
+                self.drop_features['drop_row'].append(col)
+            else:
+                pass
+
+
+if __name__ == '__main__':
+    ap = AutomaticProcess()
+    df = pd.read_csv('./data/AirQualityUCI.csv')
+
+    print(df.info())
+    ap.missing_value_process(df)
+    print(df.info())
